19684fc44f9f4e14fc8f5ad1c176cc6b7bf939f3
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2019-07-09 19:02:49 +0200

    Add serial open/close files

---
+++ serial_close.bat
@@ -0,0 +1,4 @@
+rem set path=%path%;"c:\Program Files (x86)\BEL\Realterm\"
+rem realterm.exe first quit
+
+start taskkill /f /im realterm.exe
---
+++ serial_open.bat
@@ -0,0 +1,2 @@
+set path=%path%;"c:\Program Files (x86)\BEL\Realterm\"
+realterm.exe baud=115200 port=9 display=1
