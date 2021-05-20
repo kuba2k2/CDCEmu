e3d6d1890dd6d8c16bf16b7e2d5ecad4f01959c2
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-05-20 13:08:36 +0200

    Remove code

--- serial_close.bat
+++
@@ -1,4 +0,0 @@
-rem set path=%path%;"c:\Program Files (x86)\BEL\Realterm\"
-rem realterm.exe first quit
-
-start taskkill /f /im realterm.exe
--- serial_open.bat
+++
@@ -1,2 +0,0 @@
-set path=%path%;"c:\Program Files (x86)\BEL\Realterm\"
-realterm.exe baud=115200 port=9 display=1
