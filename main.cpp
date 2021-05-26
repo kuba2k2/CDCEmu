8046756fa9376584dbaf4bc11e8ff2a6d1954e24
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-05-26 21:55:29 +0200

    Optimize packet sending code

--- main.cpp
+++ main.cpp
@@ -15,81 +15,117 @@ struct can_message msg;
 
 #define TRACK_MAX       100
 
-bool radio_enabled = 0;
-bool radio_playing = 0;
-uint8_t disk_num = 1;
-uint8_t track_num = 1;
+#define DATA_RADIO_ENABLED      0
+#define DATA_RADIO_PLAYING      1
+#define DATA_DISK_NUM           2
+#define DATA_TRACK_NUM          3
+
+uint8_t data[8] = {false, false, 1, 1, 0x00, 0x00, 0x00, 0x00};
+
+uint8_t packets[] = {
+    // 0x162, DLC 7, 2 data bytes
+    0x71, 0x62, 0b10100000, 0b00000010, 0x06, 0, 0x00, 0x06, 0x00, 2,
+        // msg[1] |= data[0] & 1 << 0
+        DATA_RADIO_PLAYING | (1<<3), 1 | (0<<3),
+        // msg[3] |= data[2] & 15 << 0
+        DATA_DISK_NUM | (0b1111<<3), 3 | (0<<3),
+
+    // 0x1A2, DLC 5, 0 data bytes
+    0x51, 0xA2, TRACK_MAX, 0x58, 0x00, 0x00, 0x00, 0,
+
+    // 0x1E2, DLC 7, 1 data byte
+    0x71, 0xE2, 0, 0x05, 0x00, 1, 1, 0x00, 0x00, 1,
+        // msg[0] |= data[3] & 0xff << 0
+        DATA_TRACK_NUM, 0 | (0<<3),
+
+    // 0x1A0, DLC 2, 0 data bytes
+    0x21, 0xA0, 0b10010010, 0b00000000, 0,
+};
+
+#define META_SIZE       3
+#define META_COUNT      4
+
+uint8_t packets_meta[META_SIZE * META_COUNT] = {
+    // packet 0x162 (packets[0x00]), timer 0, every 100ms, flag data[0]
+    0x00, (TIMER_CURRENT_DISK<<5) | T_MS(100), DATA_RADIO_ENABLED,
+    // packet 0x1A2 (packets[0x0e]), timer 1, every 500ms, flag data[1]
+    0x0e, (TIMER_TRACK_COUNT<<5) | T_MS(500), DATA_RADIO_PLAYING,
+    // packet 0x1E2 (packets[0x16]), timer 2, every 500ms, flag data[1]
+    0x16, (TIMER_TRACK_NUM<<5) | T_MS(500), DATA_RADIO_PLAYING,
+    // packet 0x1A0 (packets[0x22]), timer 3, every 500ms, flag data[1]
+    0x22, (TIMER_STATUS<<5) | T_MS(500), DATA_RADIO_PLAYING,
+};
 
 void radio_enable() {
-    radio_enabled = true;
+    data[DATA_RADIO_ENABLED] = true;
     timer_reset(TIMER_CURRENT_DISK);
     prints("radio_enable\n");
 }
 
 void radio_disable() {
-    radio_enabled = false;
+    data[DATA_RADIO_ENABLED] = false;
     prints("radio_disable\n");
 }
 
 void radio_play() {
-    radio_playing = true;
+    data[DATA_RADIO_PLAYING] = true;
     prints("radio_play\n");
 }
 
 void radio_pause() {
-    radio_playing = false;
+    data[DATA_RADIO_PLAYING] = false;
     prints("radio_pause\n");
 }
 
 void track_previous(uint8_t skip_to) {
-    if (track_num > 1)
-        track_num--;
+    if (data[DATA_TRACK_NUM] > 1)
+        data[DATA_TRACK_NUM]--;
     if (skip_to)
-        track_num = skip_to;
+        data[DATA_TRACK_NUM] = skip_to;
     prints("track_previous\n");
 }
 
 void track_next(uint8_t skip_to) {
-    if (track_num < TRACK_MAX)
-        track_num++;
+    if (data[DATA_TRACK_NUM] < TRACK_MAX)
+        data[DATA_TRACK_NUM]++;
     if (skip_to)
-        track_num = skip_to;
+        data[DATA_TRACK_NUM] = skip_to;
     prints("track_next\n");
 }
 
 void cdc_command_parse(const uint8_t cmd[8]) {
     // radio enabled (byte 0, bit 7)
-    if (((cmd[0] & (1<<7)) >> 7) != radio_enabled) {
-        if (radio_enabled)
-            radio_disable();
-        else
+    if (cmd[0] & 1<<7) {
+        if (!data[DATA_RADIO_ENABLED])
             radio_enable();
     }
+    else if (data[DATA_RADIO_ENABLED])
+        radio_disable();
 
     // single track back (byte 0, bit 6)
-    if (cmd[0] & (1<<6))
+    if (cmd[0] & 1<<6)
         track_previous(0);
 
     // single track forward (byte 0, bit 5)
-    if (cmd[0] & (1<<5))
+    if (cmd[0] & 1<<5)
         track_next(0);
 
     // back to start (byte 0, bit 3)
-    if (cmd[0] & (1<<3))
+    if (cmd[0] & 1<<3)
         track_previous(0);
 
     // playing enabled (byte 0, bit 1)
-    if (((cmd[0] & (1<<1)) >> 1) != radio_playing) {
-        if (radio_playing)
-            radio_pause();
-        else
+    if (cmd[0] & 1<<1) {
+        if (!data[DATA_RADIO_PLAYING])
             radio_play();
     }
+    else if (data[DATA_RADIO_PLAYING])
+        radio_pause();
 
     // go to track (byte 4)
-    if (cmd[4] && cmd[4] < track_num)
+    if (cmd[4] && cmd[4] < data[DATA_TRACK_NUM])
         track_previous(cmd[4]);
-    if (cmd[4] && cmd[4] > track_num)
+    if (cmd[4] && cmd[4] > data[DATA_TRACK_NUM])
         track_next(cmd[4]);
 }
 
@@ -119,49 +155,46 @@ int main() {
                 cdc_command_parse(msg.data);
         }
 
-        if (radio_enabled && timer_check(TIMER_CURRENT_DISK, T_MS(100))) {
-            msg.can_id = 0x162;
-            msg.can_dlc = 7;
-            msg.data[0] = 0b10100000;
-            msg.data[1] = 0b00000010 | (radio_playing & 1);
-            msg.data[2] = 0x06;
-            msg.data[3] = disk_num;
-            msg.data[4] = 0x00;
-            msg.data[5] = 0x06;
-            msg.data[6] = 0x00;
-            mcp_send(&msg);
-        }
+        for (uint8_t i = 0; i < META_COUNT; i++) {
+            uint8_t *meta = packets_meta + META_SIZE * i;
+            uint8_t timer = meta[1] >> 5;
+            uint8_t delay = meta[1] & 0b11111;
+            // check the specified timer
+            if (!timer_check(timer, delay))
+                continue;
+            // check the flag data byte
+            if (meta[2] != 0xff && !data[meta[2]])
+                continue;
+            // get a pointer to the packet data
+            uint8_t *packet = packets + meta[0];
 
-        if (radio_playing && timer_check(TIMER_TRACK_COUNT, T_MS(500))) {
-            msg.can_id = 0x1A2;
-            msg.can_dlc = 5;
-            msg.data[0] = TRACK_MAX;
-            msg.data[1] = 0x58;
-            msg.data[2] = 0x00;
-            msg.data[3] = 0x00;
-            msg.data[4] = 0x00;
-            mcp_send(&msg);
-        }
+            // set the ID and DLC
+            msg.can_id = ((packet[0] << 8) | packet[1]) & 0x7ff;
+            msg.can_dlc = packet[0] >> 4;
 
-        if (radio_playing && timer_check(TIMER_TRACK_NUM, T_MS(500))) {
-            msg.can_id = 0x1E2;
-            msg.can_dlc = 7;
-            msg.data[0] = track_num;
-            msg.data[1] = 0x05;
-            msg.data[2] = 0x00;
-            msg.data[3] = 1; // track minute
-            msg.data[4] = 1; // track second
-            msg.data[5] = 0x00;
-            msg.data[6] = 0x00;
-            mcp_send(&msg);
-        }
+            // set all default message payload bytes
+            for (uint8_t j = 0; j < msg.can_dlc; j++) {
+                msg.data[j] = packet[2 + j];
+            }
+            // get a pointer to the data byte part
+            packet += 2 + msg.can_dlc;
 
-        if (radio_playing && timer_check(TIMER_STATUS, T_MS(500))) {
-            msg.can_id = 0x1A0;
-            msg.can_dlc = 2;
-            msg.data[0] = 0b10010010;
-            msg.data[1] = 0b00000000;
-            mcp_send(&msg);
+            uint8_t db_count = packet[0];
+            // set all configured data bytes
+            for (uint8_t j = 0; j < db_count; j++) {
+                uint8_t src = packet[1 + j*2];
+                uint8_t dst = packet[2 + j*2];
+                uint8_t src_byte = src & 0b111;
+                uint8_t src_mask = (src>>3) ? (src>>3) : 0xff;
+                uint8_t dst_byte = dst & 0b111;
+                uint8_t dst_rsh = (dst>>3);
+                msg.data[dst_byte] |= (data[src_byte] & src_mask) << dst_rsh;
+            }
+
+            // wait until the TX buffer becomes available (one of three)
+            while (!mcp_send(&msg)) {
+                _delay_ms(1);
+            }
         }
 
         if (purx_dataready()) {
